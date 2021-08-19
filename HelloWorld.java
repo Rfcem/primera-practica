a31fd1e106212a1cdd45ade51fc37de011ed2334
Johan S Ramirez C <johan.ramirez@mercadolibre.com.co>
2021-08-19 12:05:39 -0500

    cambio en el helloword java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld{
     public static main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World - hola mundo - adios mundo - bye mundo");
     }
 }
